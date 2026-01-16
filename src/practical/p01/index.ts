a005d015655a08b400783b2a4920cfbf8960901d
68010468-Tanapat <68010468@kmitl.ac.th>
2026-01-16 10:42:01 +0700

    3

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,33 @@
-export function getEdgePosts() {}
+async function getEdgePosts() {
+  const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(`error: ${response.status}`);
+    }
+    const posts = await response.json();
+
+    if (posts.length === 0) {
+      return []; 
+    }
+    const firstPost = posts[0];
+    const lastPost = posts[posts.length - 1];
+
+    const formattedFirstPost = {
+      id: firstPost.id,
+      title: firstPost.title
+    };
+
+    const formattedLastPost = {
+      id: lastPost.id,
+      title: lastPost.title
+    };
+
+    return [formattedFirstPost, formattedLastPost];
+
+  } catch (error) {
+    return null; 
+  }
+}
+getEdgePosts()
